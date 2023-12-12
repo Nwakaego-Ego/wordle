365ca0115659924da03e0c50fa42690235182b6f
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-12-12 14:20:16 -0800

    grid color

--- src/app/Component/Board/board.jsx
+++ src/app/Component/Board/board.jsx
@@ -1,248 +1,108 @@
 "use client";
 
 import React, { useContext } from "react";
-import { AppContent } from "../../page";
+
 import Letter from "../Letter/letter";
 import "./board.css";
 
-const Board = ({ attemptVal, letterPosition }) => {
-  const { board, correctWord, position } = useContext(AppContent);
-
-  const calculateLetterState = (attemptVal, letterPosition) => {
-    if (!board || !board[attemptVal]) {
-      return "error"; // or some default state if data is not available
-    }
-
-    const letter = board[attemptVal][letterPosition];
-    const correct = correctWord[letterPosition] === letter;
-    const almost = !correct && correctWord.includes(letter);
-
-    return position.attempt > attemptVal
-      ? correct
-        ? "correct"
-        : almost
-        ? "almost"
-        : "error"
-      : "";
-  };
-
+const Board = () => {
   return (
     <div className="container-main">
       <div className="container-board">
         <span className="item ">
-          <Letter
-            letterPosition={0}
-            attemptVal={0}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={0} attemptVal={0} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={1}
-            attemptVal={0}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={1} attemptVal={0} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={2}
-            attemptVal={0}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={2} attemptVal={0} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={3}
-            attemptVal={0}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={3} attemptVal={0} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={4}
-            attemptVal={0}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={4} attemptVal={0} />
         </span>
 
         <span className="item ">
-          <Letter
-            letterPosition={0}
-            attemptVal={1}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={0} attemptVal={1} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={1}
-            attemptVal={1}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={1} attemptVal={1} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={2}
-            attemptVal={1}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={2} attemptVal={1} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={3}
-            attemptVal={1}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={3} attemptVal={1} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={4}
-            attemptVal={1}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={4} attemptVal={1} />
         </span>
 
         <span className="item ">
-          <Letter
-            letterPosition={0}
-            attemptVal={2}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={0} attemptVal={2} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={1}
-            attemptVal={2}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={1} attemptVal={2} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={2}
-            attemptVal={2}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={2} attemptVal={2} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={3}
-            attemptVal={2}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={3} attemptVal={2} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={4}
-            attemptVal={2}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={4} attemptVal={2} />
         </span>
 
         <span className="item ">
-          <Letter
-            letterPosition={0}
-            attemptVal={3}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={0} attemptVal={3} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={1}
-            attemptVal={3}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={1} attemptVal={3} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={2}
-            attemptVal={3}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={2} attemptVal={3} />
         </span>
         <span className="item">
-          <Letter
-            letterPosition={3}
-            attemptVal={3}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={3} attemptVal={3} v />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={4}
-            attemptVal={3}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={4} attemptVal={3} />
         </span>
 
         <span className="item ">
-          <Letter
-            letterPosition={0}
-            attemptVal={4}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={0} attemptVal={4} />
         </span>
         <span className="item">
-          <Letter
-            letterPosition={1}
-            attemptVal={4}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={1} attemptVal={4} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={2}
-            attemptVal={4}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={2} attemptVal={4} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={3}
-            attemptVal={4}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={3} attemptVal={4} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={4}
-            attemptVal={4}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={4} attemptVal={4} />
         </span>
 
         <span className="item ">
-          <Letter
-            letterPosition={0}
-            attemptVal={5}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={0} attemptVal={5} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={1}
-            attemptVal={5}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={1} attemptVal={5} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={2}
-            attemptVal={5}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={2} attemptVal={5} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={3}
-            attemptVal={5}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={3} attemptVal={5} />
         </span>
         <span className="item ">
-          <Letter
-            letterPosition={4}
-            attemptVal={5}
-            letterState={calculateLetterState(attemptVal, letterPosition)}
-          />
+          <Letter letterPosition={4} attemptVal={5} />
         </span>
       </div>
     </div>
--- src/app/Component/Letter/letter.jsx
+++ src/app/Component/Letter/letter.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { AppContent } from "../../page";
 import "./letter.css";
 
-const Letter = ({ attemptVal, letterPosition, letterState }) => {
+const Letter = ({ attemptVal, letterPosition }) => {
   const { board, correctWord, position } = useContext(AppContent);
 
   // Check if board and board[attemptVal] are defined
@@ -12,17 +12,17 @@ const Letter = ({ attemptVal, letterPosition, letterState }) => {
 
   const letter = board[attemptVal][letterPosition];
 
-  // // Check if the current letter is correct
-  // const correct = correctWord[letterPosition] === letter;
+  // Check if the current letter is correct
+  const correct = correctWord[letterPosition] === letter;
 
-  // // Check if the current letter is almost correct
-  // const almost = !correct && correctWord.includes(letter);
+  // Check if the current letter is almost correct
+  const almost = !correct && correctWord.includes(letter);
 
-  // // Determine the letter state based on conditions
-  // let letterState = "";
-  // if (position.attempt > attemptVal) {
-  //   letterState = correct ? "correct" : almost ? "almost" : "error";
-  // }
+  // Determine the letter state based on conditions
+  let letterState = "";
+  if (position.attempt > attemptVal) {
+    letterState = correct ? "correct" : almost ? "almost" : "error";
+  }
 
   return <div className={`letter-row text-white ${letterState}`}>{letter}</div>;
 
